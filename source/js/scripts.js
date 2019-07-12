dd3ae9e7932495354434a5f01609a020b7864eea
Aileen <aileen.kingsler@mail.ru>
2019-07-12 17:09:30 +1000

    Добавляет вёрстку блока О нас и футера

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,9 +1,22 @@
 $(function () {
 
-  setTimeout(function(){
+  var toTopBtn = $('.to-top');
+
+  setTimeout(function () {
     $('.calc-form__select').styler();
   }, 100);
 
   $('.examples__photos').slick();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600) toTopBtn.fadeIn();
+    else toTopBtn.fadeOut();
+  });
+
+  toTopBtn.click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 700);
+  });
 
 });
