332c231dd27f27fc44a7506fc7498c2835bb8b9e
Aileen <aileen.kingsler@mail.ru>
2019-07-14 18:50:18 +1000

    Добавляет медиазапросы для (max-width: 767px)

--- gulpfile.js
+++ gulpfile.js
@@ -109,6 +109,7 @@ gulp.task("server", function () {
   gulp.watch("source/img/*", gulp.series("copy", "refresh"));
   gulp.watch("source/img/sprite/*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*", gulp.series("scripts", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,12 +2,6 @@ $(function () {
 
   var toTopBtn = $('.to-top');
 
-  setTimeout(function () {
-    $('.calc-form__select').styler();
-  }, 100);
-
-  $('.examples__photos').slick();
-
   $(window).scroll(function () {
     if ($(this).scrollTop() > 600) toTopBtn.fadeIn();
     else toTopBtn.fadeOut();
@@ -17,6 +11,54 @@ $(function () {
     $('body, html').animate({
       scrollTop: 0
     }, 700);
+  });
+
+  setTimeout(function () {
+    $('.calc-form__select').styler();
+  }, 100);
+
+  $('.examples__photos').slick();
+
+  $('.sertificates__list').slick({
+    slidesToShow: 4,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+    ]
+  });
+
+  $('.testimonials__list').slick({
+    slidesToShow: 3,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+    ]
+  });
+
+  $('.mistakes__list').slick({
+    slidesToShow: 3,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+    ]
   });
 
 });
